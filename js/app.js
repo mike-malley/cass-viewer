7942962d440e702d36129646d796a2e3c3227ef5
Ronald Ray <fritz.ray@eduworks.com>
2017-09-15 14:45:31 -0700

    Communicates with parent page, works in an iframe.

--- js/app.js
+++ js/app.js
@@ -22,6 +22,11 @@ queryParams = function () {
     }
     return {};
 };
+queryParams = queryParams();
+if (queryParams.select == "true")
+    $("#selectButton").show();
+if (queryParams.select != null)
+    $("#selectButton").show().text(queryParams.select);
 
 var repo = new EcRepository();
 repo.selectedServer = "";
@@ -30,10 +35,10 @@ EcRepository.caching = true;
 
 var frameworkId = "";
 
-var servers = ["https://sandbox.cassproject.org/api/custom", "https://dev.cassproject.org/api/custom"];
+var servers = ["https://sandbox.cassproject.org/api/custom"];
 
-if (queryParams().server != null)
-    servers = [queryParams().server];
+if (queryParams.server != null)
+    servers = [queryParams.server];
 
 for (var i = 0; i < servers.length; i++) {
     var r = new EcRepository();
@@ -50,17 +55,20 @@ function refreshFrameworks() {
         searchTerm = "*";
     var me = this;
     me.loading = 0;
-    $("#sidebar").find("#loading").show();
+    $("#sidebar").show().find("#loading").show().find("#status").text("Loading...");
     $("#sidebar").find("a,br").remove();
     for (var i = 0; i < servers.length; i++) {
         loading++;
         EcFramework.search(servers[i], searchTerm, function (frameworks) {
             for (var v = 0; v < frameworks.length; v++) {
-                $("#frameworks").append("<p><a style='display:none'/></p>").children().last().children().last().attr("id", frameworks[v].shortId()).text(frameworks[v].name).click(click);
+                var framework = frameworks[v];
+                if (framework.name === undefined || framework.name == null || framework.name == "")
+                    continue;
+                $("#frameworks").append("<p><a style='display:none'/><span/></p>").children().last().children().first().attr("id", framework.shortId()).text(framework.name).click(click).parent().children().last().text(framework.description);
             }
             loading--;
             if (loading == 0) {
-                $("#sidebar").find("#loading").hide();
+                $("#sidebar").find("#loading").hide().find("#status").text("Please select a framework.");
                 $("#sidebar").find("a").show();
             }
         }, console.log, {
@@ -69,9 +77,18 @@ function refreshFrameworks() {
     }
 }
 
+function select() {
+    var ary = [];
+    $("input:checked").parent().each(function (f) {
+        ary.push($(this).attr("id"));
+    })
+    parent.postMessage(ary, queryParams.origin);
+}
+
 function click(evt) {
     hideAll();
     frameworkId = $(evt.target).attr("id");
+    $("#sidebar").hide();
     repo = null;
     for (var i = 0; i < servers.length; i++)
         if (frameworkId.startsWith(servers[i])) {
@@ -92,73 +109,76 @@ function refreshFramework() {
     me.fetches = 0;
     EcRepository.get(frameworkId, function (framework) {
         $("#title").text(framework.name);
+        $("#mainbar").show();
         if (framework.competency == null)
             framework.competency = [];
         if (framework.relation == null)
             framework.relation = [];
-        $("#frameworkLink").attr("href", framework.shortId()).show();
-        //repo.precache(framework.competency.concat(framework.relation), function (success) {
-        if (framework.competency.length == 0)
-            showAll();
-        else
-            for (var i = 0; i < framework.competency.length; i++) {
-                me.fetches++;
-                EcCompetency.get(framework.competency[i], function (competency) {
-                    me.fetches--;
-                    var treeNode = $("#tree").append("<li class = 'competency'><ul></ul></li>").children().last();
-                    treeNode.attr("id", competency.shortId());
-                    if (competency.description != null && competency.description != "NULL" && competency.description != competency.name)
-                        treeNode.prepend("<br><small>" + competency.description + "</small>");
-                    treeNode.prepend(" <a target='_blank' href='" + competency.shortId() + "'>🔗</a>");
-                    treeNode.prepend("<span>" + competency.name + "</span>").children().first().click(function (evt) {
-                        $(evt.target).parent().children("ul").slideToggle();
-                    });
-                    treeNode.prepend("<input type='checkbox'>").children().first().click(function (evt) {
-                        console.log(evt);
-                        $(evt.target).parent().find("input").prop("checked", evt.target.checked);
-                    });
-                    if (me.fetches == 0) {
-                        if (framework.relation != undefined && framework.relation.length > 0) {
-                            for (var i = 0; i < framework.relation.length; i++) {
-                                me.fetches++;
-                                EcAlignment.get(framework.relation[i], function (relation) {
-                                    me.fetches--;
-                                    if (relation.source !== undefined) {
-                                        if (relation.relationType == "narrows") {
-                                            $("[id=\"" + relation.target + "\"]").children().last().append($("[id=\"" + relation.source + "\"]"));
-                                        }
-                                        if (me.fetches == 0) {
-                                            for (var i = 0; i < framework.relation.length; i++) {
-                                                me.fetches++;
-                                                EcAlignment.get(framework.relation[i], function (relation) {
-                                                    me.fetches--;
-                                                    if (relation.source !== undefined) {
-                                                        if (relation.relationType == "requires") {
-                                                            if ($("[id=\"" + relation.target + "\"]").prevAll("[id=\"" + relation.source + "\"]").length > 0)
-                                                                $("[id=\"" + relation.target + "\"]").insertBefore($("[id=\"" + relation.source + "\"]"));
+        if (queryParams.link == "true")
+            $("#frameworkLink").attr("href", framework.shortId()).show();
+        repo.precache(framework.competency.concat(framework.relation), function (success) {
+            if (framework.competency.length == 0)
+                showAll();
+            else
+                for (var i = 0; i < framework.competency.length; i++) {
+                    me.fetches++;
+                    EcCompetency.get(framework.competency[i], function (competency) {
+                        me.fetches--;
+                        var treeNode = $("#tree").append("<li class = 'competency'><ul></ul></li>").children().last();
+                        treeNode.attr("id", competency.shortId());
+                        if (competency.description != null && competency.description != "NULL" && competency.description != competency.name)
+                            treeNode.prepend("<small>" + competency.description + "</small>");
+                        if (queryParams.link == "true")
+                            treeNode.prepend(" <a target='_blank' href='" + competency.shortId() + "'>🔗</a>");
+                        treeNode.prepend("<span>" + competency.name + "</span>").children().first().click(function (evt) {
+                            $(evt.target).parent().children("ul").slideToggle();
+                        });
+                        if (queryParams.select  != null)
+                            treeNode.prepend("<input type='checkbox'>").children().first().click(function (evt) {
+                                console.log(evt);
+                                $(evt.target).parent().find("input").prop("checked", evt.target.checked);
+                            });
+                        if (me.fetches == 0) {
+                            if (framework.relation != undefined && framework.relation.length > 0) {
+                                for (var i = 0; i < framework.relation.length; i++) {
+                                    me.fetches++;
+                                    EcAlignment.get(framework.relation[i], function (relation) {
+                                        me.fetches--;
+                                        if (relation.source !== undefined) {
+                                            if (relation.relationType == "narrows") {
+                                                $("[id=\"" + relation.target + "\"]").children().last().append($("[id=\"" + relation.source + "\"]"));
+                                            }
+                                            if (me.fetches == 0) {
+                                                for (var i = 0; i < framework.relation.length; i++) {
+                                                    me.fetches++;
+                                                    EcAlignment.get(framework.relation[i], function (relation) {
+                                                        me.fetches--;
+                                                        if (relation.source !== undefined) {
+                                                            if (relation.relationType == "requires") {
+                                                                if ($("[id=\"" + relation.target + "\"]").prevAll("[id=\"" + relation.source + "\"]").length > 0)
+                                                                    $("[id=\"" + relation.target + "\"]").insertBefore($("[id=\"" + relation.source + "\"]"));
+                                                            }
                                                         }
-                                                    }
-                                                    if (me.fetches == 0) {
-                                                        showAll();
-                                                    }
-                                                }, console.log);
+                                                        if (me.fetches == 0) {
+                                                            showAll();
+                                                        }
+                                                    }, console.log);
+                                                }
                                             }
                                         }
-                                    }
-                                }, console.log);
-                            }
-                        } else
-                            showAll();
-                    }
-                }, console.log);
-            }
-            //});
+                                    }, console.log);
+                                }
+                            } else
+                                showAll();
+                        }
+                    }, console.log);
+                }
+        });
     }, console.log);
 }
 
 
 function showAll() {
-    $("button").show();
     $("#main").find("#loading").hide();
     $("#tree").show();
     $("#sidetitle").show();
@@ -173,7 +193,6 @@ function showAll() {
 
 function hideAll() {
     $('.done').show();
-    $("button").hide();
     $("#sidetitle").hide();
     $("#sidelog").hide();
     $("#sidedevicestitle").hide();
