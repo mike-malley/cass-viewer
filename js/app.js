56fac0632bf530277d6decc763243ac13c3fbdd1
Ronald Ray <fritz.ray@eduworks.com>
2017-09-15 16:23:17 -0700

    Style importing.

--- js/app.js
+++ js/app.js
@@ -199,11 +199,33 @@ $("#search").keyup(function (event) {
 
 window.onload = function () {
     if (parent) {
+        importParentStyles();
         var oHead = document.getElementsByTagName("head")[0];
         var arrStyleSheets = parent.document.getElementsByTagName("style");
         for (var i = 0; i < arrStyleSheets.length; i++)
             oHead.appendChild(arrStyleSheets[i].cloneNode(true));
     }
+}
+
+function importParentStyles() {
+    var parentStyleSheets = parent.document.styleSheets;
+    var cssString = "";
+    for (var i = 0, count = parentStyleSheets.length; i < count; ++i) {
+        if (parentStyleSheets[i].cssRules) {
+            var cssRules = parentStyleSheets[i].cssRules;
+            for (var j = 0, countJ = cssRules.length; j < countJ; ++j)
+                cssString += cssRules[j].cssText;
+        } else
+            cssString += parentStyleSheets[i].cssText; // IE8 and earlier
+    }
+    var style = document.createElement("style");
+    style.type = "text/css";
+    try {
+        style.innerHTML = cssString;
+    } catch (ex) {
+        style.styleSheet.cssText = cssString; // IE8 and earlier
+    }
+    document.getElementsByTagName("head")[0].appendChild(style);
 }
 
 refreshFrameworks();
