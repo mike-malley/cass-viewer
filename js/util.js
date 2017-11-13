a611d7c04e4e404653f0662967429083740612e7
Ronald Ray <fritz.ray@eduworks.com>
2017-11-13 14:46:19 -0800

    CE theme, updated style-pulling.

--- js/util.js
+++ js/util.js
@@ -26,15 +26,20 @@ queryParams = function () {
 };
 queryParams = queryParams();
 
-window.onload = function () {
+$(document).ready(function () {
     if (parent != window) {
-        importParentStyles();
         var oHead = document.getElementsByTagName("head")[0];
         var arrStyleSheets = parent.document.getElementsByTagName("style");
         for (var i = 0; i < arrStyleSheets.length; i++)
             oHead.appendChild(arrStyleSheets[i].cloneNode(true));
+        arrStyleSheets = parent.document.getElementsByTagName("link");
+        for (var i = 0; i < arrStyleSheets.length; i++)
+            oHead.appendChild(arrStyleSheets[i].cloneNode(true));
+        try {
+            importParentStyles();
+        } catch (e) {}
     }
-}
+});
 
 function importParentStyles() {
     var parentStyleSheets = parent.document.styleSheets;
